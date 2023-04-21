4c21d79ae7d5e10fdea134bc85dabc67798a26d2
Frederick Bruce <frederick.bruce.dev@gmail.com>
2023-04-21 15:45:44 -0400

    Endpoint Testing
    API is working in console as expected.

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
+import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
   const [article, setArticle] = useState({
@@ -7,9 +8,23 @@ const Demo = () => {
     summary: "",
   });
 
+  // RTK lazy query
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  
   const handleSubmit = async (e) => {
-    alert("Submitted");
-  }
+    e.preventDefault();
+
+    const { data } = await getSummary({ articleUrl: article.url });
+
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+
+      setArticle(newArticle);
+
+      console.log(newArticle);
+    }
+  };
 
   return (
     <section className="mt-16 w-full max-w-xl">
@@ -29,7 +44,7 @@ const Demo = () => {
             type="url"
             placeholder="Enter a URL"
             value={article.url}
-            onChange={(e) => setArticle({ ... article, url: e.target.value })}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
             required
             className="url_input peer"
           />
--- src/services/article.js
+++ src/services/article.js
@@ -1,27 +1,27 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const RapidApiKey = import.meta.env.VITE_RAPID_API_KEY;
+const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 
 export const articleApi = createApi({
   reducerPath: "articleApi",
   baseQuery: fetchBaseQuery({
     baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
     prepareHeaders: (headers) => {
-      headers.set("X-RapidAPI-Key", RapidApiKey);
+      headers.set("X-RapidAPI-Key", rapidApiKey);
       headers.set(
         "X-RapidAPI-Host",
         "article-extractor-and-summarizer.p.rapidapi.com"
       );
 
       return headers;
-    }
+    },
   }),
   endpoints: (builder) => ({
     getSummary: builder.query({
-      query: (params) => `/summarize?url=$
-      {encodeURIComponent(params.articleUrl)}&length=3`
-    })
-  })
+      query: (params) =>
+        `summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
+    }),
+  }),
 });
 
 // use hook on demand when user submits url
