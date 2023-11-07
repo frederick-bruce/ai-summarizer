7f145f849a33d8df5fd4654345d2368b9db2aa09
Frederick Bruce <frederick.bruce.dev@gmail.com>
2023-11-07 14:02:58 -0500

    grab existing articles from local storage

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -26,12 +26,18 @@ const Demo = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const { data } = await getSummary({ articleUrl: article.url });
+    const existingArticle = allArticles.find(
+      (item) => item.url === article.url
+    );
 
+    if (existingArticle) return setArticle(existingArticle);
+
+    const { data } = await getSummary({ articleUrl: article.url });
     if (data?.summary) {
       const newArticle = { ...article, summary: data.summary };
-
       const updatedAllArticles = [newArticle, ...allArticles];
+
+      // update state and local storage
 
       setArticle(newArticle);
       setAllArticles(updatedAllArticles);
@@ -42,11 +48,13 @@ const Demo = () => {
     }
   };
 
+  // copy url and toggle icon
+
   const handleCopy = (copyUrl) => {
     setCopied(copyUrl);
     navigator.clipboard.writeText(copyUrl);
-    setTimeout(() => setCopied(false), 3000)
-  }
+    setTimeout(() => setCopied(false), 3000);
+  };
 
   return (
     <section className="mt-16 w-full max-w-xl">
@@ -88,11 +96,11 @@ const Demo = () => {
               className="link_card"
             >
               <div className="copy_btn" onClick={() => handleCopy(item.url)}>
-                <img 
+                <img
                   src={copied === item.url ? tick : copy}
                   alt="copy_icon"
                   className="w-[40%] h-[40%] object-contain"
-               />
+                />
               </div>
               <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
                 {item.url}
@@ -101,30 +109,33 @@ const Demo = () => {
           ))}
         </div>
       </div>
+
       {/* Display Results */}
       <div className="my-10 max-w-full flex justify-center items-center">
-            {isFetching ? (
-              <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
-            ) : error ? (
-              <p className="font-inter font-bold text-black text-center">
-                "Oops, that was not supposed to happen..."
-                <br />
-                <span className="font-satoshi font-normal text-gray-700">
-                  {error?.data?.error}
-                </span>
-              </p>
-            ) : (
-              article.summary && (
-                <div className="flex flex-col gap-3">
-                  <h2 className="font-satoshi font-bold text-gray-600 text-xl">
-                    Article <span className="blue_gradient">Summary</span>
-                  </h2>
-                  <div className="summary_box">
-                    <p className="font-inter font-medium font-sm text-gray-700">{article.summary}</p>
-                  </div>
-                </div>
-              )
-            )}
+        {isFetching ? (
+          <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+        ) : error ? (
+          <p className="font-inter font-bold text-black text-center">
+            "Oops, that was not supposed to happen..."
+            <br />
+            <span className="font-satoshi font-normal text-gray-700">
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                Article <span className="blue_gradient">Summary</span>
+              </h2>
+              <div className="summary_box">
+                <p className="font-inter font-medium font-sm text-gray-700">
+                  {article.summary}
+                </p>
+              </div>
+            </div>
+          )
+        )}
       </div>
     </section>
   );
