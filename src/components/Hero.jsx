8ee5094d643e245170be6a4be1ff7cc64f96d7a4
Frederick Bruce <frederick.bruce.dev@gmail.com>
2023-11-07 15:11:48 -0500

    change logo name

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
       <nav className="flex justify-between items-center w-full mb-10 pt-3">
-        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
+        <h1 className="text-3xl orange_gradient">AI Summarizer</h1>
 
         <button
           type="button"
@@ -21,7 +21,8 @@ const Hero = () => {
         <span className="orange_gradient">OpenAI GPT-4</span>
       </h1>
       <h2 className="desc">
-        Simplify your reading with Summize, an open-source article summarizer that transforms lengthy articles into clear and concise summaries. 
+        Simplify your reading with Summize, an open-source article summarizer
+        that transforms lengthy articles into clear and concise summaries.
       </h2>
     </header>
   );
